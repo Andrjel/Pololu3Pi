da467bf526ae64b22f69162545bb30de4ca0dc8f
Marcin Kazmierczak <km48913@zut.edu.pl>
2023-03-29 11:12:28 +0200

    Set test TCP server for data transmission

--- Pololu3Pi.py
+++ Pololu3Pi.py
@@ -2,6 +2,7 @@
 import socket
 import tkinter.messagebox
 from tkinter import *
+import time
 
 
 # Making tkinter window class
@@ -20,41 +21,51 @@ class App:
         self.ipFrame = Frame(master, width=100, height=50, bg="grey")
         self.ipFrame.place(x=120, y=0)
         # opis pola
-        self.ipLabel = Label(self.ipFrame, text='Ip and connection')
+        self.ipLabel = Label(self.ipFrame, text='Insert IP and connect')
         self.ipLabel.grid(column=0, row=0, padx=5, pady=5, columnspan=2)
         # Przycisk do ustalenia polaczenia
-        self.connectionButton = Button(self.ipFrame, text="Connect", padx=40, pady=20,command=self.ConnectionButton)
+        self.connectionButton = Button(self.ipFrame, text="Connect", padx=40, pady=20, command=self.ConnectionButton)
         self.connectionButton.grid(column=1, row=1, padx=5, pady=5)
         # Pole do wprowadzenia koncowki ip
         self.ipEntry = Entry(self.ipFrame, width=3)
         self.ipEntry.grid(column=0, row=1, padx=5, pady=5)
-    
-    # przycisk połączenia
+
+    # Connect button
     def ConnectionButton(self):
         try:
             koncowkaIp = int(self.ipEntry.get())
         except ValueError:
             koncowkaIp = None
-        
-        if koncowkaIp is None or not (30 < koncowkaIp < 40):
-            tkinter.messagebox.showinfo("Connection Status", "Niepoprawny numer ip!")
-        else:
-            ipRobota = "192.168.2." + str(koncowkaIp)
-            # print(ipRobota)
-        
 
+        if koncowkaIp is None or not (30 < koncowkaIp < 40):
+            tkinter.messagebox.showinfo("Connection Status", "Wrong ip!")
+        else:
+            # ipRobota = "192.168.2." + str(koncowkaIp)
+            # For now Im gonna use local ip for testing
+            ipRobota = "127.0.0.1"
+            port = 8000
+            # print(ipRobota)
+            my_socket.connect((ipRobota, port))
+            # start data transmission
+            self.DataTransmission()
+
+    # Data transmission
+    def DataTransmission(self):
+        robotControlData = b"[010000]"
+        my_socket.send(robotControlData)
+        time.sleep(1)
+        self.recvdata = my_socket.recv(1024).decode()
+        print(self.recvdata)
+        self.ipFrame.after(1000, self.DataTransmission)
 
 
 if __name__ == "__main__":
     # Create a Socket
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # port 8000
-    port = 8000
-    # connecting to the mobile robot server
-    # my_socket.connect(('127.0.0.1', port))
     # Making window root
     root = Tk()
     root.title('Pololu3Pi')
     root.geometry('800x500')
+    # calling gui
     app = App(root)
     root.mainloop()
